e57cc863c20b0346d46aa8a24833956b555af0a7
Adil <adilzhapar2002@gmail.com>
2022-03-03 12:01:34 +0600

    upd

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,74 +2,67 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.example.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var math_operation: TextView
-    private lateinit var result: TextView
-    private lateinit var btn_0: TextView
-    private lateinit var btn_1: TextView
-    private lateinit var btn_2: TextView
-    private lateinit var btn_3: TextView
-    private lateinit var btn_4: TextView
-    private lateinit var btn_5: TextView
-    private lateinit var btn_6: TextView
-    private lateinit var btn_7: TextView
-    private lateinit var btn_8: TextView
-    private lateinit var btn_9: TextView
-
-    private lateinit var btn_pls: TextView
-    private lateinit var btn_mns: TextView
-    private lateinit var btn_mlp: TextView
-    private lateinit var btn_div: TextView
-    private lateinit var btn_back: TextView
+    private lateinit var binding: ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        math_operation = findViewById(R.id.math_operation)
-        btn_0 = findViewById(R.id.btn_0)
-        btn_1 = findViewById(R.id.btn_1)
-        btn_2 = findViewById(R.id.btn_2)
-        btn_3 = findViewById(R.id.btn_3)
-        btn_4 = findViewById(R.id.btn_4)
-        btn_5 = findViewById(R.id.btn_5)
-        btn_6 = findViewById(R.id.btn_6)
-        btn_7 = findViewById(R.id.btn_7)
-        btn_8 = findViewById(R.id.btn_8)
-        btn_9 = findViewById(R.id.btn_9)
+        binding.btn0.setOnClickListener { textFields("0") }
+        binding.btn1.setOnClickListener { textFields("1") }
+        binding.btn2.setOnClickListener { textFields("2") }
+        binding.btn3.setOnClickListener { textFields("3") }
+        binding.btn4.setOnClickListener { textFields("4") }
+        binding.btn5.setOnClickListener { textFields("5") }
+        binding.btn6.setOnClickListener { textFields("6") }
+        binding.btn7.setOnClickListener { textFields("7") }
+        binding.btn8.setOnClickListener { textFields("8") }
+        binding.btn9.setOnClickListener { textFields("9") }
 
-        btn_pls = findViewById(R.id.btn_pls)
-        btn_mns = findViewById(R.id.btn_mns)
-        btn_mlp = findViewById(R.id.btn_mlp)
-        btn_div = findViewById(R.id.btn_div)
-        btn_back = findViewById(R.id.btn_back)
+        binding.btnCom.setOnClickListener { textFields(".") }
+        binding.btnPls.setOnClickListener { textFields("+") }
+        binding.btnMns.setOnClickListener { textFields("-") }
+        binding.btnMlp.setOnClickListener { textFields("*") }
+        binding.btnDiv.setOnClickListener { textFields("/") }
 
-        btn_0.setOnClickListener {textFields("0")}
-        btn_1.setOnClickListener {textFields("1")}
-        btn_2.setOnClickListener {textFields("2")}
-        btn_3.setOnClickListener {textFields("3")}
-        btn_4.setOnClickListener {textFields("4")}
-        btn_5.setOnClickListener {textFields("5")}
-        btn_6.setOnClickListener {textFields("6")}
-        btn_7.setOnClickListener {textFields("7")}
-        btn_8.setOnClickListener {textFields("8")}
-        btn_9.setOnClickListener {textFields("9")}
-        btn_mns.setOnClickListener {textFields("-")}
-        btn_pls.setOnClickListener {textFields("+")}
-        btn_mlp.setOnClickListener {textFields("\u2715")}
-        btn_div.setOnClickListener {textFields("\u00F7")}
+        binding.btnEq.setOnClickListener {
+            try {
 
-        btn_back.setOnClickListener {
-            val str = math_operation.text.toString()
+                val ex = ExpressionBuilder(binding.mathOperation.text.toString()).build()
+                val result = ex.evaluate()
+
+                val longRes = result.toLong()
+                if(result == longRes.toDouble()){
+                    binding.result.text = longRes.toString()
+                }else{
+                    binding.result.text = binding.result.toString()
+                }
+            } catch (e:Exception) {
+                Log.d("Error", "message: ${e.message}")
+            }
+        }
+
+        binding.btnBack.setOnClickListener {
+            val str = binding.mathOperation.text.toString()
             if(str.isNotEmpty())
-                math_operation.text = str.substring(0, str.length-1)
+                binding.mathOperation.text = str.substring(0, str.length-1)
         }
     }
 
     fun textFields(str: String){
-        math_operation.append(str);
+        if(binding.result.text != ""){
+            binding.mathOperation.text = binding.result.text
+            binding.result.text = ""
+        }
+        binding.mathOperation.append(str)
     }
 }
