4ee7ff3cc5baa804a8cc3a5c880233c157c3cf2d
Adil <adilzhapar2002@gmail.com>
2022-03-08 23:03:55 +0600

    upd

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
 import com.example.calculator.databinding.ActivityMainBinding
 import net.objecthunter.exp4j.ExpressionBuilder
 import java.lang.Exception
@@ -11,54 +10,57 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btn0.setOnClickListener { textFields("0") }
-        binding.btn1.setOnClickListener { textFields("1") }
-        binding.btn2.setOnClickListener { textFields("2") }
-        binding.btn3.setOnClickListener { textFields("3") }
-        binding.btn4.setOnClickListener { textFields("4") }
-        binding.btn5.setOnClickListener { textFields("5") }
-        binding.btn6.setOnClickListener { textFields("6") }
-        binding.btn7.setOnClickListener { textFields("7") }
-        binding.btn8.setOnClickListener { textFields("8") }
-        binding.btn9.setOnClickListener { textFields("9") }
+        initUI()
+    }
 
-        binding.btnCom.setOnClickListener { textFields(".") }
-        binding.btnPls.setOnClickListener { textFields("+") }
-        binding.btnMns.setOnClickListener { textFields("-") }
-        binding.btnMlp.setOnClickListener { textFields("*") }
-        binding.btnDiv.setOnClickListener { textFields("/") }
+    private fun initUI() {
+        with(binding) {
+            btn0.setOnClickListener { textFields("0") }
+            btn1.setOnClickListener { textFields("1") }
+            btn2.setOnClickListener { textFields("2") }
+            btn3.setOnClickListener { textFields("3") }
+            btn4.setOnClickListener { textFields("4") }
+            btn5.setOnClickListener { textFields("5") }
+            btn6.setOnClickListener { textFields("6") }
+            btn7.setOnClickListener { textFields("7") }
+            btn8.setOnClickListener { textFields("8") }
+            btn9.setOnClickListener { textFields("9") }
+            btnCom.setOnClickListener { textFields(".") }
+            btnPls.setOnClickListener { textFields("+") }
+            btnMns.setOnClickListener { textFields("-") }
+            btnMlp.setOnClickListener { textFields("*") }
+            btnDiv.setOnClickListener { textFields("/") }
 
-        binding.btnEq.setOnClickListener {
-            try {
+            btnEq.setOnClickListener {
+                try {
+                    val ex = ExpressionBuilder(mathOperation.text.toString()).build()
+                    val res = ex.evaluate()
 
-                val ex = ExpressionBuilder(binding.mathOperation.text.toString()).build()
-                val res = ex.evaluate()
-
-                val longRes = res.toLong()
-                if(res == longRes.toDouble()){
-                    binding.result.text = longRes.toString()
-                }else{
-                    binding.result.text = res.toString()
+                    val longRes = res.toLong()
+                    if(res == longRes.toDouble()) {
+                        result.text = longRes.toString()
+                    } else{
+                        result.text = res.toString()
+                    }
+                } catch (e:Exception) {
+                    Log.d("Error", "message: ${e.message}")
                 }
-            } catch (e:Exception) {
-                Log.d("Error", "message: ${e.message}")
             }
-        }
 
-        binding.btnBack.setOnClickListener {
-            val str = binding.mathOperation.text.toString()
-            if(str.isNotEmpty())
-                binding.mathOperation.text = str.substring(0, str.length-1)
+            btnBack.setOnClickListener {
+                val str = mathOperation.text.toString()
+                if(str.isNotEmpty())
+                    mathOperation.text = str.substring(0, str.length-1)
+            }
         }
     }
 
-    fun textFields(str: String){
+    private fun textFields(str: String) {
         if(binding.result.text != ""){
             binding.mathOperation.text = binding.result.text
             binding.result.text = ""
