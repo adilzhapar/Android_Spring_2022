a466b6751125b1e520ee6c0acb55d33f9a8b5d9d
Adil <adilzhapar2002@gmail.com>
2022-04-04 15:46:58 +0600

    to-do w java

---
+++ ToDo/app/src/main/java/com/example/to_do/Model/ToDoModel.java
@@ -0,0 +1,32 @@
+package com.example.to_do.Model;
+
+public class ToDoModel {
+    private int id, status;
+    private String task;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+
+}
---
+++ ToDo/app/src/main/java/com/example/to_do/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.to_do;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(SavedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
+    }
+}
