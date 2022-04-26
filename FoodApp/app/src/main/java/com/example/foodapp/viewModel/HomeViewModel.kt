c82b36dadb2b11ff4ff1eb001f13c140af3dc15c
Adil <adilzhapar2002@gmail.com>
2022-04-26 23:29:30 +0600

    FoodApp

---
+++ FoodApp/app/src/main/java/com/example/foodapp/viewModel/HomeViewModel.kt
@@ -0,0 +1,59 @@
+package com.example.foodapp.viewModel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.foodapp.fragments.HomeFragment
+import com.example.foodapp.pojo.CategoryList
+import com.example.foodapp.pojo.CategoryMeal
+import com.example.foodapp.pojo.Meal
+import com.example.foodapp.pojo.MealList
+import com.example.foodapp.retrofit.RetrofitInstance
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class HomeViewModel(): ViewModel() {
+    private var randomMealLiveData = MutableLiveData<Meal>()
+    private var popularItemsLiveData = MutableLiveData<List<CategoryMeal>>()
+
+    fun getRandomMeal(){
+        RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList> {
+            override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
+                if(response.body() != null){
+                    val randomMeal: Meal = response.body()!!.meals[0]
+                    randomMealLiveData.value = randomMeal
+                }else{
+                    return
+                }
+            }
+
+            override fun onFailure(call: Call<MealList>, t: Throwable) {
+                Log.d("HomeFragment", t.message.toString())
+            }
+        })
+    }
+
+    fun getPopularItems(){
+        RetrofitInstance.api.getPopularItems("Seafood").enqueue(object: Callback<CategoryList>{
+            override fun onResponse(call: Call<CategoryList>, response: Response<CategoryList>) {
+                if(response.body() != null){
+                    popularItemsLiveData.value = response.body()!!.meals
+                }
+            }
+
+            override fun onFailure(call: Call<CategoryList>, t: Throwable) {
+                Log.d("HomeFragment", t.message.toString())
+            }
+        })
+    }
+
+    fun observeRandomMealLiveData():LiveData<Meal>{
+        return randomMealLiveData
+    }
+
+    fun observePopularItemsLiveData():LiveData<List<CategoryMeal>>{
+        return popularItemsLiveData
+    }
+}
